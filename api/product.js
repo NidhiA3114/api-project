7d8f3fbfff68c09d80c7e4b2bedac6076ab0d37e
NidhiA3114 <anidhi130@gmail.com>
2024-10-11 11:19:12 +0530

    change the API data

--- api/product.js
+++ api/product.js
@@ -14,7 +14,7 @@ export default function handler(req, res) {
 
     // Respond with the product data for GET requests
     res.status(200).json([
-        { id: 1, name: 'Product 1', price: 10 },
-        { id: 2, name: 'Product 2', price: 20 },
+        { id: 1, name: 'Tshirt', price: 1000 },
+        { id: 2, name: 'Top', price: 2000},
     ]);
 }
